6a4645c78dd07fd035d1ccf6e8c4285297dfcdbc
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2023-03-23 04:06:12 +0300

    Add files via upload

--- crypto.py
+++ crypto.py
@@ -17,28 +17,25 @@ from error import (
     ZeroShiftException,
 )
 
-_ALPHABET_LEN = len(_ALPHABET := string.printable)
+ALPHABET_LEN = len(ALPHABET := string.printable)
 
 # = Custom Type Hints =
-_TIntArray = NDArray[np.int32]
-
-_TJob = Literal['+', '-']
-
-_TOrd = Literal['ij', 'ji']
-
+TJob = Literal['+', '-']
+TOrder = Literal['ij', 'ji']
+TIntArray = NDArray[np.int32]
 # https://www.tcl.tk/man/tcl/TkCmd/entry.html#M16
-_TVCMDCode = Literal['%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W']
+TVCMDCode = Literal['%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W']
 
 # = Validate Actions =
-_DELETE = '0'
-_INSERT = '1'
+DELETE = '0'
+INSERT = '1'
 
 
-class _Cipher(ABC):
+class Cipher(ABC):
     """Abstract base class for cipher classes."""
 
     name: str
-    code: tuple[_TVCMDCode, _TVCMDCode]
+    code: tuple[TVCMDCode, TVCMDCode]
 
     def __init__(self, text: str):
         self._text = text
@@ -54,7 +51,7 @@ class _Cipher(ABC):
 
     @staticmethod
     @abstractmethod
-    def validate(action: _TVCMDCode, data: _TVCMDCode) -> bool:
+    def validate(action: TVCMDCode, data: TVCMDCode) -> bool:
         """Validate command."""
 
     @abstractmethod
@@ -66,7 +63,7 @@ class _Cipher(ABC):
         """Decrypt."""
 
 
-class _NotACipher(_Cipher):
+class NotACipher(Cipher):
     """Not a cipher."""
 
     name = ''
@@ -90,7 +87,7 @@ class _NotACipher(_Cipher):
         return self.text
 
 
-class _Caesar(_Cipher):
+class Caesar(Cipher):
     """Caesar cipher."""
 
     name = 'Caesar'
@@ -101,23 +98,23 @@ class _Caesar(_Cipher):
 
         self._key = int(key)
 
-        if (self._key % _ALPHABET_LEN) == 0:
+        if (self._key % ALPHABET_LEN) == 0:
             raise ZeroShiftException('Key error. Shift value is equal to 0.')
 
     @staticmethod
     def validate(action, data):
         """Validate command."""
-        return (action == _DELETE) or data.isdigit()
+        return (action == DELETE) or data.isdigit()
 
     def encrypt(self):
         """Encrypt."""
-        return self._do_it('+')
+        return self._do('+')
 
     def decrypt(self):
         """Decrypt."""
-        return self._do_it('-')
+        return self._do('-')
 
-    def _do_it(self, job: _TJob) -> str:
+    def _do(self, job: TJob) -> str:
         """Encrypt/decrypt."""
         jobs = {
             '+': operator.add,
@@ -127,15 +124,15 @@ class _Caesar(_Cipher):
         text = ''
 
         for char in self.text:
-            c_idx_text = _ALPHABET.index(char)
+            c_idx_text = ALPHABET.index(char)
             c_idx_key = self._key
 
-            text += _ALPHABET[jobs[job](c_idx_text, c_idx_key) % _ALPHABET_LEN]
+            text += ALPHABET[jobs[job](c_idx_text, c_idx_key) % ALPHABET_LEN]
 
         return text
 
 
-class _Hill(_Cipher):
+class Hill(Cipher):
     """Hill cipher."""
 
     name = 'Hill'
@@ -152,22 +149,22 @@ class _Hill(_Cipher):
 
         if determinant == 0:
             raise MatrixNotInvertibleException('Key matrix is not invertible.')
-        if math.gcd(determinant, _ALPHABET_LEN) != 1:
+        if math.gcd(determinant, ALPHABET_LEN) != 1:
             raise NotCoPrimeException(
                 'Key determinant and alphabet length are not co-prime.'
             )
 
         self._m_adj = np.linalg.inv(self._key) * determinant
 
-        self._det_inv = pow(determinant, -1, _ALPHABET_LEN)
+        self._det_inv = pow(determinant, -1, ALPHABET_LEN)
 
     @staticmethod
     def validate(action, data):
         """Validate command."""
-        return (action == _DELETE) or (data in _ALPHABET)
+        return (action == DELETE) or (data in ALPHABET)
 
     @staticmethod
-    def _m_fill(vals: str, shape: tuple[int, int], order: _TOrd) -> _TIntArray:
+    def _m_fill(vals: str, shape: tuple[int, int], order: TOrder) -> TIntArray:
         """Create a new matrix and fill it."""
         orders = {
             'ij': lambda *given: given,
@@ -186,12 +183,12 @@ class _Hill(_Cipher):
                 fill += 1
                 continue
 
-            matrix[orders[order](i, j)] = _ALPHABET.index(vals[idx])
+            matrix[orders[order](i, j)] = ALPHABET.index(vals[idx])
             idx += 1
 
         return matrix
 
-    def _m_multiply(self, matrix: NDArray) -> _TIntArray:
+    def _m_multiply(self, matrix: NDArray) -> TIntArray:
         """Multiply the given matrix by the column vectors."""
         col = math.ceil(len(self.text) / self._row)
 
@@ -200,20 +197,20 @@ class _Hill(_Cipher):
         m_multiplied = np.matmul(matrix.astype(int), vectors)
         m_transposed = np.transpose(m_multiplied)
 
-        return np.concatenate(m_transposed) % _ALPHABET_LEN
+        return np.concatenate(m_transposed) % ALPHABET_LEN
 
     def encrypt(self):
         """Encrypt."""
-        return ''.join(_ALPHABET[_] for _ in self._m_multiply(self._key))
+        return ''.join(ALPHABET[_] for _ in self._m_multiply(self._key))
 
     def decrypt(self):
         """Decrypt."""
         m_inv = np.array(np.around(self._m_adj * self._det_inv))
 
-        return ''.join(_ALPHABET[_] for _ in self._m_multiply(m_inv))
+        return ''.join(ALPHABET[_] for _ in self._m_multiply(m_inv))
 
 
-class _Scytale(_Cipher):
+class Scytale(Cipher):
     """Scytale cipher."""
 
     name = 'Scytale'
@@ -226,7 +223,7 @@ class _Scytale(_Cipher):
     @staticmethod
     def validate(action, data):
         """Validate command."""
-        return (action == _DELETE) or bool(re.match(r'[1-9]\d*$', data))
+        return (action == DELETE) or bool(re.match(r'[1-9]\d*$', data))
 
     def encrypt(self):
         """Encrypt."""
@@ -250,7 +247,7 @@ class _Scytale(_Cipher):
         return ''.join(text)
 
 
-class _Vigenere(_Cipher):
+class Vigenere(Cipher):
     """Vigenère cipher."""
 
     name = 'Vigenère'
@@ -263,17 +260,17 @@ class _Vigenere(_Cipher):
     @staticmethod
     def validate(action, data):
         """Validate command."""
-        return (action == _DELETE) or (data in _ALPHABET)
+        return (action == DELETE) or (data in ALPHABET)
 
     def encrypt(self):
         """Encrypt."""
-        return self._do_it('+')
+        return self._do('+')
 
     def decrypt(self):
         """Decrypt."""
-        return self._do_it('-')
+        return self._do('-')
 
-    def _do_it(self, job: _TJob) -> str:
+    def _do(self, job: TJob) -> str:
         """Encrypt/decrypt."""
         jobs = {
             '+': operator.add,
@@ -285,18 +282,18 @@ class _Vigenere(_Cipher):
         text = ''
 
         for char in self.text:
-            c_idx_text = _ALPHABET.index(char)
-            c_idx_key = _ALPHABET.index(next(key))
+            c_idx_text = ALPHABET.index(char)
+            c_idx_key = ALPHABET.index(next(key))
 
-            text += _ALPHABET[jobs[job](c_idx_text, c_idx_key) % _ALPHABET_LEN]
+            text += ALPHABET[jobs[job](c_idx_text, c_idx_key) % ALPHABET_LEN]
 
         return text
 
 
 ciphers = {
-    (NAC := _NotACipher.name): _NotACipher,
-    (CAESAR := _Caesar.name): _Caesar,
-    (HILL := _Hill.name): _Hill,
-    (SCYTALE := _Scytale.name): _Scytale,
-    (VIGENERE := _Vigenere.name): _Vigenere,
-}  # type: dict[str, type[_Cipher]]
+    (NAC := NotACipher.name): NotACipher,
+    (CAESAR := Caesar.name): Caesar,
+    (HILL := Hill.name): Hill,
+    (SCYTALE := Scytale.name): Scytale,
+    (VIGENERE := Vigenere.name): Vigenere,
+}  # type: dict[str, type[Cipher]]
